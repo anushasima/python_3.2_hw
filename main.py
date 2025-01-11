132e6decadd3027f92732418a0cce304ceb22d00
anushasima <0939713580@ukr.net>
2025-01-11 20:18:23 +0200

    added fixes

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-import math
 
-my_list = [12, 3, 4, 10]
-# my_list.append(33)
-# my_list.insert(0, False)
-# my_list.extend([111, 222])
-# my_list.reverse()
-my_list.pop(3)
-my_list.insert(0, 10)
-# k = 0
-# my_list[-1], my_list[k] = my_list[k], my_list[-1]
-print(my_list)
+def last_to_first(lst):
+    if not lst:
+        return lst
+    if len(lst) == 1:
+        return lst
 
-my_list = [1]
-my_list.pop(0)
-my_list.insert(0, 1)
-print(my_list)
+    last_element = lst.pop()
+    lst.insert(0, last_element)
+    return lst
 
-my_list = [12, 3, 4, 10, 8]
-my_list.pop(4)
-my_list.insert(0, 8)
-print(my_list)
+my_list = [1,2,3,4,9]
+# a = int(input("create list and print some mumbers: "))
+result = last_to_first(my_list)
+print(result)
 
-
-# з пустим списком складнощі адже нема чого переміщувати
-my_list = []
-my_list.pop()
-my_list.insert(0)
-print(my_list)
